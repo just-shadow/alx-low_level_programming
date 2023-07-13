1761a698a5820bc5f115eda058fc85f568b6ade8
Sammy Shoka <sammyshoka@gmail.com>
2023-07-12 23:49:46 -0700

    My commit for 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point for a program that prints a line with puts function
+ * Return:0 It means the program has been succesfully implemented
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle.\n");
+	return(0);
+}
