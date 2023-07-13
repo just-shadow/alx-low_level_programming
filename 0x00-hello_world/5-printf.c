7bec5fa5f05607cbfacff964787317d5d5f2dfdf
Sammy Shoka <sammyshoka@gmail.com>
2023-07-13 00:05:48 -0700

    My commit for 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -Entry point for a program that prints a line using printf function
+ * Return: 0 Means the program has been succesfully implemented
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
