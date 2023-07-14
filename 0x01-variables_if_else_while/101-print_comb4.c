a13208bf1c162247deecca234fa11ea8a1870acc
Sammy Shoka <sammyshoka@gmail.com>
2023-07-14 10:39:13 -0700

    My commit for 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,19 +9,19 @@ int main(void)
 
 	int m;
 
-	int 1;
+	int z;
 
 	for (n = 48; n < 58; n++)
 	{
 		for (m = 49; m < 58; m++)
 		{
-			for (l = 50; l < 58; l++)
+			for (z = 50; z < 58; z++)
 			{
-				if (l > m && m > n)
+				if (z > m && m > n)
 				{
 					putchar(n);
 					putchar(m);
-					putchar(l);
+					putchar(z);
 					if (n != 55 || m != 56)
 					{
 						putchar(',');
