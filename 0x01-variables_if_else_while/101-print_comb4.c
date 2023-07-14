f77680e6ef195291dbb613167065f70c147c5dc0
Sammy Shoka <sammyshoka@gmail.com>
2023-07-14 10:33:59 -0700

    My commit for 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return:0 Prgram succesful implemented
+ */
+int main(void)
+{
+	int n;
+
+	int m;
+
+	int 1;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
+			for (l = 50; l < 58; l++)
+			{
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
