06c9ca7fe226cf6074b1caa7d55ec2bf113adbb7
Sammy Shoka <sammyshoka@gmail.com>
2023-07-14 01:56:52 -0700

    My commit for 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -Entry point for a program
+ * return (0) -Program implemented succesful
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 12; n < 22; n++)
+	{
+		putchar(n);
+	}
+	putchar("\n");
+	return (0);
+}
