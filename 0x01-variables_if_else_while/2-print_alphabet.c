f32c94373d0d4591283c48c4e90abd4e248fe6d8
Sammy Shoka <sammyshoka@gmail.com>
2023-07-14 10:59:07 -0700

    My commit for 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-	char ch;
+	char c;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		putchar(ch);
+		putchar(c);
 	}
 	putchar('\n');
 	return (0);
