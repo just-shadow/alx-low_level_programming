4b17d4da389903fd2082b1d53b695b8b24c60f93
Sammy Shoka <sammyshoka@gmail.com>
2023-07-14 10:28:03 -0700

    My commit for 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,15 +7,15 @@ int main(void)
 {
 	int n, m;
 
-	for (n = 12; n <= 14; n++)
+	for (n = 48; n <= 56; n++)
 	{
-		for (m = 13; m <= 15; m++)
+		for (m = 49; m <= 57; m++)
 		{
 			if (m > n)
 			{
 				putchar(n);
 				putchar(m);
-				if (n != 14 || m != 15)
+				if (n != 56 || m != 57)
 				{
 					putchar(',');
 					putchar(' ');
