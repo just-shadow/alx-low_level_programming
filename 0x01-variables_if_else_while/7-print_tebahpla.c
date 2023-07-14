e227ed9e583de5801c3a796474a312e7273567ca
Sammy Shoka <sammyshoka@gmail.com>
2023-07-14 02:01:42 -0700

    My commit for 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -Entry point for a program
+ * return (0) -Program implemented succesful
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch == 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar("\n");
+	return (0);
+}
