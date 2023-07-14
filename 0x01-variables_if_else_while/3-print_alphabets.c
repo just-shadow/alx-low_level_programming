82305d16ffa5bb4b1f5dfd3f9a3e009cf800910a
Sammy Shoka <sammyshoka@gmail.com>
2023-07-14 09:45:51 -0700

    My commit for 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
  * main -Entry point for a program
- * return (0) -Program implemented succesfully
+ * Return:0 -Program implemented succesfully
  */
 int main(void)
 {
 	int ch;
 
-	for (ch = 'a'; ch = 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
-	for (ch = 'A'; ch = 'Z'; ch++)
+	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar(ch);
 	putchar("\n");
 	return (0);
