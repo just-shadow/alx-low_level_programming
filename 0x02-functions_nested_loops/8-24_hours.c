ae501ccf33a1fb9d97004638c9dee7b9c9c4484e
Sammy Shoka <sammyshoka@gmail.com>
2023-07-18 04:43:47 -0700

    My commit for 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ */
+void jack_bauer(void)
+{
+	int n, m;
+
+	n = 0;
+	while (n < 24)
+	{
+		m = 0;
+		while (m < 60)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+			m++;
+		}
+		n++;
+	}
+}
