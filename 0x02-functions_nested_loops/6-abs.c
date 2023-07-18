e06a41ca45edc481d623f5efd2f5e93c383b135d
Sammy Shoka <sammyshoka@gmail.com>
2023-07-18 03:56:34 -0700

    My commit for 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - Computes absolute value of integer
+ * @s: The int to check
+ * Return: Absolute value of int
+ */
+int _abs(int s)
+{
+	if (s > 0)
+		return (s);
+	else
+		return (-s);
+}
