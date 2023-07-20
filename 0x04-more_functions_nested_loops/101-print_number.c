8231e6b5dea96285a976813243d6f95fdf823298
Sammy Shoka <sammyshoka@gmail.com>
2023-07-20 06:14:58 -0700

    My commit for 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_number -  prints an integer
+ * @n: integer to print
+ */
+void print_number(int n)
+{
+	unsigned int m = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		m = n;
+		_putchar('_');
+	}
+	m /= 10;
+	if (m != 0)
+		print_number(m);
+	_putchar((unsigned int) n % 10 + '0');
+}
