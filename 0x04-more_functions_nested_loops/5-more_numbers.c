6d40b14334859ffc00bae954a1f1eb24b03a5ebf
Sammy Shoka <sammyshoka@gmail.com>
2023-07-20 06:37:46 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,16 +8,13 @@ void more_numbers(void)
 	int i;
 
 	i = 0;
-	while (i < 0)
+	while (i < 10)
 	{
 		j = 0;
 		while (j <= 14)
 		{
-			if (j < 9)
-			{
-				_putchar((j / 10) + '0');
-			}
-			_putchar((j % 10) + '0');
+			_putchar((j / 10 + '0'));
+			_putchar((j % 10 + '0'));
 			j++;
 		}
 		_putchar('\n');
