d50c4ce0029a44adf0fac01fd0e6fe4b1892c3f2
Sammy Shoka <sammyshoka@gmail.com>
2023-07-24 07:18:29 -0700

    My commit for 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string to get lenght of
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		length++;
+	}
+	return (length);
+}
