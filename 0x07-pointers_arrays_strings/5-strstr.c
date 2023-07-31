a548b355bf2a0036cde779804ae491d8b870b6aa
Sammy Shoka <sammyshoka@gmail.com>
2023-07-31 12:23:02 -0700

    My commit for 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: the string to be checked
+ * @needle: substring to be located
+ * Return: 0 seccess
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *m = haystack;
+		char *n = needle;
+
+		while (*m == *n && *n != '\0')
+		{
+			m++;
+			n++;
+		}
+		if (*p == '\0')
+			return (haystack);
+	}
+	return (0);
+}
