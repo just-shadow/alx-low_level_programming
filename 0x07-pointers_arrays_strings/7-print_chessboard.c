263a2f382ceebc3ce65ccf5ec43c849c11b1603c
Sammy Shoka <sammyshoka@gmail.com>
2023-07-31 12:32:51 -0700

    My commit for 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard
+ * @a: array
+ * Return: 0 success
+ */
+void print_chessboard(char (*a)[8])
+{
+	int m, n;
+
+	for (m = 0; m < 8; m++)
+	{
+		for (n = 0; n < 8; n++)
+			_putchar(a[m][n]);
+		_putchar('\n');
+	}
+}
