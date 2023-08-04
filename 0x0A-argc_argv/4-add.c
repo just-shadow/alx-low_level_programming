387243eccfc4ea3fc8bbda56cca8d6e6957d5b61
Sammy Shoka <sammyshoka@gmail.com>
2023-08-04 07:25:03 -0700

    My commit for 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+	unsigned int y, sum = 0;
+	char *z;
+
+	if (argc > 1)
+	{
+		for (x = 1; x < argc; x++)
+		{
+			z = argv[x];
+			for (y = 0; y < strlen(z); y++)
+			{
+				if (z[y] < 0 || z[y] > 9)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(z);
+			z++;
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
+}
