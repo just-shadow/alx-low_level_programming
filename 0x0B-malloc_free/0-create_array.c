967e17c8ec3c6e98b784e9d6c3c43b3891326203
Sammy Shoka <sammyshoka@gmail.com>
2023-08-08 04:04:51 -0700

    My commit for 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,7 +6,7 @@
  * @c: char to fill array
  * Return: array filled
  */
-char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c)
 {
 	char *s;
 	unsigned int i;
