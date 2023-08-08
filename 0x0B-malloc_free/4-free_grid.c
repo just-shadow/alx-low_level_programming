dc2d4d3e7ae6b1862e36936753e9a2ee3dbab6b8
Sammy Shoka <sammyshoka@gmail.com>
2023-08-08 04:00:48 -0700

    My commit for 4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - fress 2 dimensional array
+ * @grid: 2dimensional grid
+ * @height: height dimension of grid
+ * Return: 0 success
+ */
+void free_grid(int **grid, int height);
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
